d31d3ac557a6569f5a0f708683e2917043654f1f
DaivaGi <daiva.gik@gmail.com>
2026-01-07 14:46:09 +0200

    Initial project setup with Spring Boot, H2 and Flyway

---
+++ src/main/java/lt/daiva/bankstatement/BankStatementApplication.java
@@ -0,0 +1,22 @@
+package lt.daiva.bankstatement;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Bank Statement API",
+				version = "1.0",
+				description = "Service for importing/exporting bank statements and calculating balances"
+		)
+)
+@SpringBootApplication
+public class BankStatementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankStatementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/daiva/bankstatement/BankstatementApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.daiva.bankstatement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankstatementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
